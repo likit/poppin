6a1e0f0320529b01ee8266a731b67f8c14eeefea
Likit Preeyanon <preeyano@msu.edu>
2014-01-08 13:00:55 -0500

    update rename-contigs.py

--- rename-contigs.py
+++ rename-contigs.py
@@ -3,12 +3,11 @@ import glob
 import os
 
 for f in glob.glob(os.path.join(sys.argv[1], '*.fa.cap.contigs')):
-    geneid = f.split('.')[2].split('_')[-1]
     tmp = '%s.tmp' % f
     tmpfile = open(tmp, 'w')
     for line in open(f):
         if line.startswith('>'):
-            line = line.strip('\n').replace('>', '>' + geneid + '_')
+            line = line.strip('\n').replace('>', '>' + f + '_')
             print >> tmpfile, line
         else:
             print >> tmpfile, line.strip('\n')
