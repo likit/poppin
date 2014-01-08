1251bc0980dcd4f1ec48952d23d89a4956276c8f
Likit Preeyanon <preeyano@msu.edu>
2014-01-08 12:39:25 -0500

    add rename-contigs.py

---
+++ rename-contigs.py
@@ -0,0 +1,15 @@
+import sys
+import glob
+import os
+
+for f in glob.glob(os.path.join(sys.argv[1], '*cap.fa.contigs')):
+    geneid = f.split('.')[2].split('_')[-1]
+    tmp = '%s.tmp' % f
+    tmp = open(tmp, 'w')
+    for line in open(f):
+        if line.startswith('>'):
+            line.strip('\n').replace('Contig', geneid + '-')
+            print >> tmp, line
+        else:
+            print >> tmp, line.strip('\n')
+    tmp.close()
