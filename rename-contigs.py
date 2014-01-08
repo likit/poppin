7270fa543eee8902ef432c582b00debc5a96a096
Likit Preeyanon <preeyano@msu.edu>
2014-01-08 13:04:44 -0500

    update rename-contigs.py

--- rename-contigs.py
+++ rename-contigs.py
@@ -5,9 +5,10 @@ import os
 for f in glob.glob(os.path.join(sys.argv[1], '*.fa.cap.contigs')):
     tmp = '%s.tmp' % f
     tmpfile = open(tmp, 'w')
+    seqid = os.path.split(f)[-1]
     for line in open(f):
         if line.startswith('>'):
-            line = line.strip('\n').replace('>', '>' + f + '_')
+            line = line.strip('\n').replace('>', '>' + seqid + '_')
             print >> tmpfile, line
         else:
             print >> tmpfile, line.strip('\n')
