c148eeb353d7e9734b061073d9149f6d25eca258
Likit Preeyanon <preeyano@msu.edu>
2014-01-10 19:49:13 -0500

    update concat-seq.py

--- concat-seq.py
+++ concat-seq.py
@@ -5,19 +5,20 @@ from Bio.SeqRecord import SeqRecord
 '''Concatenate sequences from all input files to create
 a supercontig/gene.
 
-Usage: concat-seq.py <input files> <seqid>
+Usage: concat-seq.py <seqid> <input files>
 
 '''
-
+seqid = sys.argv[1]
 superseq = SeqRecord(seq='')  # empty sequence
 numseq = 0
-for infile in sys.argv[1:]:
+for infile in sys.argv[2:]:
     print >> sys.stderr, 'working on %s' % infile
     for rec in SeqIO.parse(infile, 'fasta'):
         superseq += rec
         numseq += 1
 
-superseq.id = "supercontig"
+superseq.id = seqid
+superseq.description = ''
 SeqIO.write(superseq, sys.stdout, 'fasta')
 
 print >> sys.stderr, 'Total sequences: %d' % numseq
