cd5d5cf0c23bd03e458831ca9ac453e9fae84618
Likit Preeyanon <preeyano@msu.edu>
2014-01-10 19:15:15 -0500

    add find_max.py

---
+++ find_max.py
@@ -0,0 +1,19 @@
+import sys
+from Bio import SeqIO
+
+
+def find_max(infile):
+    max_length = None
+    for rec in SeqIO.parse(infile):
+        print rec.id, len(rec.seq)
+        if not max_length:
+            max_length = rec
+        else:
+            if len(max_length.seq) < len(rec.seq):
+                max_length = rec
+
+    print "The longest sequence is", max_length.id, len(max_length.seq)
+
+
+if __name__=='__main__':
+    find_max(sys.argv[1])
