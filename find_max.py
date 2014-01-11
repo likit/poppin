9f5189acefaf0607117b706d752670598d3688a1
Likit Preeyanon <preeyano@msu.edu>
2014-01-10 19:53:00 -0500

    update find_max.py

--- find_max.py
+++ find_max.py
@@ -1,6 +1,10 @@
 import sys
 from Bio import SeqIO
 
+'''Finds the longest sequence in a FASTA file and writes
+it to stdout.
+
+'''
 
 def find_max(infile):
     max_length = None
@@ -13,8 +17,9 @@ def find_max(infile):
                 max_length = rec
 
     # print "The longest sequence is", max_length.id, len(max_length.seq)
-    SeqIO.write(max_length, infile + '.longest', 'fasta')
+    SeqIO.write(max_length, sys.stdout, 'fasta')
 
 
 if __name__=='__main__':
-    find_max(sys.argv[1])
+    for infile in sys.argv[1:]:
+        find_max(infile)
